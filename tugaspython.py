876dd2288045e8771015c6e32437bf478ffa6218
frezer99 <31909147+frezer99@users.noreply.github.com>
2017-09-27 08:45:50 +0700

    04217018
    Tugas Algoritma Dan Pemrograman

---
+++ tugaspython.py
@@ -0,0 +1,8 @@
+namaku = "Edith Arwansyah"
+nimku = "04217018"
+umurku = 19
+
+print("Namaku = " +namaku)
+print("Nimku = " +nimku)
+print("Umurku = " ,umurku)
+print("Umurku 5 Tahun Lagi = %s"% (umurku+5))
